210a9eb4d7509774a3900c1fab6d9165b120abde
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-15 01:50:20 +0100

    observale manip in the modal service and app component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ModalService } from './services/modal.service';
 
 
 @Component({
@@ -6,7 +8,91 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
+private abc:Subscription;
+  constructor(private modalServ: ModalService) {
+
+  }
+
+  ngOnInit(): void {
+    /* 
+     let observer = {
+       next: (data: string) => {
+         console.log(data);
+       },
+       error: (err: string) => {
+         console.log(err);
+       },
+       complete: () => {
+         console.log('ads finised!')
+       },
+     };
+     this.modalServ.getsheduledAds(2).subscribe(observer)
+ 
+    ============================
+ 
+     this.modalServ.getsheduledAds(2).subscribe({
+       next: (data: string) => {
+         console.log(data);
+       },
+       error: (err: string) => {
+         console.log(err);
+       },
+       complete: () => {
+         console.log('ads finised!')
+       },
+     }
+ 
+ 
+ 
+     )
+    =====================
+   this.modalServ.getsheduledAds(2).subscribe({
+       next: (data: string) => {
+         console.log(data);
+       }
+     }
+ 
+ 
+    */
+
+
+
+
+   /*  this.modalServ.getsheduledAds(2).subscribe({
+      next: (data: string) => {
+        console.log(data);
+      },
+      error: (err: string) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('ads finised!')
+      },
+    } */
+
+    let abc: Subscription =   this.modalServ.getsheduledAds(2).subscribe({
+      next: (data: string) => {
+        console.log(data);
+      },
+      error: (err: string) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('ads finised!')
+      },
+    }
+    )
+    abc.unsubscribe()
+
+  }
+
+  ngOnDestroy(): void {
+    this.abc.unsubscribe();
+    
+  }
 
 }
- 
+
+
+/* unsubscription must be declared either in the ngondostroy or after the declaration of the observable */
--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { clearImmediate } from 'timers';
 
 interface IModal {
   id: string;
@@ -13,8 +15,54 @@ export class ModalService {
 
   private modals: IModal[] = [];
 
+  private adsList: string[];
+  constructor() {
+    this.adsList = [
+      "ads1",
+      "ads2",
+      "ads3",
+      "ads4",
+      "ads5",
+      "",
+      "ads6",
+    ];
+  }
 
-  constructor() { }
+  getsheduledAds(intervalInseconds: number): Observable<string> {
+    return new Observable<string>((observer) => {
+      /*  observer.next();
+       observer.error();
+       observer.complete(); */
+      let counter = 0;
+      let adsTimer = setInterval(() => {
+        console.log("from setinterval");
+        observer.next(this.adsList[counter]);
+        counter++;
+
+        if (counter == this.adsList.length) {
+          observer.complete();
+        }
+        if (this.adsList[counter] == "") {
+          observer.error("Error: Empty ADS");
+        }
+        
+      }, intervalInseconds * 1000);
+
+      return{
+        unsubscribe() {
+          // will be called :
+          /* 1- error
+          2- complete
+          3- unsubscribe */
+          clearInterval(adsTimer);
+          
+        },
+      }
+
+
+
+    });
+  }
 
 
   //solve the memory leak
@@ -57,5 +105,15 @@ export class ModalService {
     }
 
   }
+
+
+  // observable
+
+
+
+
+
+
+
 
 }
