8735b7d0f051146421c6377d56ffcb290eb54562
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-19 19:08:43 +0100

    build a custom validator and factory function inside the match method

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
 
 
 
@@ -33,7 +34,7 @@ export class RegisterComponent {
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber,
 
-  });
+  },[RegisterValidators.match('password','confirm_password')]);
 
   showAlert = false;
   alertMsg = 'Please Wait! Your account is being created...';
