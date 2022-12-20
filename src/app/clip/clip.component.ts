9be0ebe89a9f3b86b266f5fc8a343dc31739dbdb
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-20 11:03:21 +0100

    retrieve the id found at the end of the clip url

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clip',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clip.component.css']
 })
 export class ClipComponent implements OnInit {
-
-  constructor() { }
+id = '';
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
   }
 
 }
