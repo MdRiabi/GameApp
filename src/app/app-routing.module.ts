5ce76c21bce60b53d0c7619497318bf030a727b2
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-20 10:59:02 +0100

    html for the clip component and fixing a route for it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ClipComponent } from './clip/clip.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -9,6 +10,9 @@ const routes: Routes = [
 },
 {
   path: 'about' , component: AboutComponent
+},
+{
+  path:'clip/:id' , component:ClipComponent
 }
 
 ];
